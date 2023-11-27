7bcaefd2126e6465bbf4f5024d59fac769007bca
Ryo Takeuchi <develop@tatsutakein.jp>
2023-11-27 22:40:59 +0900

    :wrench: コードフォーマットの設定ファイルを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    sitemap(),
+  ],
+});
